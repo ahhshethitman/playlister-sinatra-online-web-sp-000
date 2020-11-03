0c18fe0d50956219ef3a89cbbcd18b232121f6c4
Hunter Sides <hunter.s.1337@gmail.com>
2020-11-03 22:26:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2020_11_03_221320) do
 
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "song_genres", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "genre_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+  end
+
 end
