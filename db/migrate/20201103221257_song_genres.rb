bca7c860d056a9d2dbc92a37992663b88ba54d2a
Hunter Sides <hunter.s.1337@gmail.com>
2020-11-03 22:23:52 +0000

    Automatically backed up by Learn

---
+++ db/migrate/20201103221257_song_genres.rb
@@ -0,0 +1,4 @@
+class SongGenres < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_03_221320) do
 
 end
